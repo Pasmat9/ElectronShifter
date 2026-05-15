aef1d55fe661b21f38b8ac2b4087579e59fb59e6
Pasmat9 <pastor.mat@email.cz>
2026-05-15 17:32:12 +0200

    p

--- Hitparáda/src/preLoad.js
+++
@@ -1,9 +0,0 @@
-//Soubor který přeposílá data k uložení mezi main a render scriptem
-
-const { contextBridge, ipcMain, ipcRenderer } = require("electron");
-
-
-contextBridge.exposeInMainWorld("Bridge", {
-    ulozData: (data) => ipcRenderer.send("ulozData", data),
-    nacistData: () => ipcRenderer.invoke("nacistData")
-});
